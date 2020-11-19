e920935b2f14e06f61c34c8ab9a83e2f761209ce
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-11-19 03:33:27 +0000

    Form Submission using JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,87 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+
+$(document).ready(() => {
+
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1261113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      let $Newtweet = createTweetElement(tweet);
+    $('#tweets-container').append($Newtweet);
+
+    }
+
+  }
+
+  const createTweetElement = function(tweet) {
+
+        const date = `${Math.round((Date.now() - Number(tweet.created_at))/(1000*3600*24))} days ago`;
+        let  $tweet= (`<article class="tweet">
+
+        <header class='tweetsHeader'>
+          <div class='RightHeader'>
+            <img class='avatar' src="https://i.imgur.com/nlhLi3I.png" >
+            <div id="userName">${tweet.user.name}</div>
+          </div>
+          <div id="handler">${tweet.user.handle}</div>
+        </header>
+
+        <div class='tweet-content'>
+          <p>${tweet.content.text}</p>
+        </div>
+
+        <footer>
+          <p>${date}</p>
+          <div class='tweet-icons'>   
+          </div>
+        </footer>
+        </article> `);
+
+        return $tweet;
+
+  }
+
+  renderTweets(tweetData);
+
+
+      $('form').on('submit', event => {
+        event.preventDefault();
+      //console.log($('form').serialize());
+      $.ajax({
+        url : "/tweets/", 
+        method :"POST",
+        data : $('form').serialize()
+      })
+        .then (res => console.log(res));
+
+  });
+
+
+          
+
+
+});
+
 
